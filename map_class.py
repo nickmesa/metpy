6bc9fd2c11d4f3a78e723adc43e4b635992ff44e
Nick Mesa <nickmesa@Nicks-MacBook-Pro.local>
2021-10-15 00:15:08 -0400

    updated class files

--- map_class.py
+++ map_class.py
@@ -6,43 +6,39 @@ import cartopy.feature as cfeature
 
 class Map:
 
-    fig = plt.figure(1, figsize = (14,12))
-    ax = plt.subplot(1,1,1, projection = ccrs.PlateCarree())
-
-    def __init__(self): 
-        pass
+    def __init__(self, extent, projection = ccrs.PlateCarree()):
+        self.fig = plt.figure(1, figsize = (1280/72, 720/72))        
+        self.ax = plt.subplot(1,1,1, projection = ccrs.PlateCarree())
+        self.ax.add_feature(cfeature.COASTLINE.with_scale('50m'))
+        self.ax.add_feature(cfeature.STATES.with_scale('10m'))
+        self.ax.add_feature(cfeature.OCEAN.with_scale('50m'))
+        self.ax.add_feature(cfeature.LAND.with_scale('50m'))
+        self.ax.set_extent(extent, projection)
     
-    @classmethod
-    def title(cls, my_title):
-        plt.title(label = f'{my_title}')
-
-    @classmethod
-    def legend(cls,colors):
-        plt.legend(cat_plot_colors)
+    def title(self, my_title):
+        self.title = plt.title(label = my_title)
     
-Map.fig
-Map.ax
+    def savefig(self, filename):
+        self.save = plt.savefig(filename)
+
+    def showfig(self):
+        self.show = plt.show()
+
+new_map = Map([-100, -10, 5, 42])
 
 cat_gdf = geopandas.read_file('https://www.nhc.noaa.gov/xgtwo/gtwo_shapefiles.zip')
 
 cat_plot_colors = {'Low': 'yellow', 'Medium': 'orange', 'High': 'red'}
 
-Map.ax.set_extent([-100, -10, 5, 42], ccrs.PlateCarree())
-
-Map.ax.add_feature(cfeature.COASTLINE.with_scale('50m'))
-Map.ax.add_feature(cfeature.STATES.with_scale('50m'))
-Map.ax.add_feature(cfeature.OCEAN.with_scale('50m'))
-Map.ax.add_feature(cfeature.LAND.with_scale('50m'))
-
 for key in cat_plot_colors.keys():
     geometries = cat_gdf[cat_gdf['RISK5DAY'] == key]
     if len(geometries) > 0:
-        Map.ax.add_geometries(geometries['geometry'], crs = ccrs.PlateCarree(), facecolor = cat_plot_colors[key], edgecolor = 'black', alpha = 0.5)
+        new_map.ax.add_geometries(geometries['geometry'], crs = ccrs.PlateCarree(), facecolor = cat_plot_colors[key], edgecolor = 'black', alpha = 0.5)
 
-Map.title('National Hurricane Center Tropical Weather Outlook')
+new_map.title('National Hurricane Center Tropical Weather Outlook')
 
-Map.legend(cat_plot_colors)
+#new_map.legend(cat_plot_colors)
 
-plt.savefig('TWO.png')
+new_map.savefig('TWO.png')
 
-plt.show()
+new_map.showfig()
